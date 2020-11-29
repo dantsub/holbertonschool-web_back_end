c1f401e0ff1b85dc2c29d65b3438ce23729436b4
dantsub <dany.mrtinez@gmail.com>
2020-11-29 13:01:58 -0500

    [Upload] first files

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('0-index.html')
