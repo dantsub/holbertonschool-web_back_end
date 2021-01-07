bffcc3a137d7177578f393aade6a6cd852f8cc70
dantsub <dany.mrtinez@gmail.com>
2021-01-07 16:07:12 -0500

    [Fix] process function instead console.log

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
